94e2b7bc0fc056a3b764cccfc702db23cf892b07
Liam Hinds <liam.hinds@kentec.co.uk>
2025-05-12 11:55:34 +0100

    initial project setup

---
+++ QaBank/BankAccount.cs
@@ -0,0 +1,32 @@
+﻿namespace QaBank;
+
+public class BankAccount
+{
+    public BankAccount()
+    {
+        
+    }
+    public string CustomerName { get; set; } = null!;
+    public int AccountNumber { get; set; }
+    public decimal Balance { get; set; } = 0M;
+
+    public void Deposit(decimal amount)
+    {
+        Balance += amount;
+    }
+
+    public bool Withdraw(decimal amount)
+    {
+        if (Balance < amount)
+            return false;
+
+        Balance -= amount;
+
+        return true;
+    }
+}
+
+public class CurrentAccount()
+{
+
+}
